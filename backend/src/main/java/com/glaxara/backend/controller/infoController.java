286651cf8ca70bf4929add0079bd87cf23540d91
Mateo <mateo.taruschio@arba.gov.ar>
2026-05-22 16:32:15 -0300

    Ajustamos horarios reales

--- backend/src/main/java/com/glaxara/backend/controller/infoController.java
+++ backend/src/main/java/com/glaxara/backend/controller/infoController.java
@@ -3,12 +3,11 @@ package com.glaxara.backend.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "${app.cors.origins}")
+
 @RestController
 @RequestMapping("/api")
 
@@ -18,7 +17,7 @@ public class infoController {
     public Map<String, String> getInfo(){
         Map<String, String> info = new HashMap<>();
 
-        info.put("horarios", "Lunes a Viernes de 9:00 a 13:00 hs y de 16:00 a 19:00 hs. Sábados de 9:00 a 13:00 hs. Domingos cerrado.");
+        info.put("horarios", "Lunes a Viernes de 7:30 a 13:30 hs y de 16:30 a 19:30 hs. Sábados de 10:00 a 13:00 hs. Domingos cerrado.");
         info.put("direccion", "https://maps.app.goo.gl/aUt7ptFJBFfZBqVv7");
 
         return info;
